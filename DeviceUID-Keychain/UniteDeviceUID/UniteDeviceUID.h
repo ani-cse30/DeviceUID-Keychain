f6b63610f03693591656c5e4e3a6e67fa35a170b
Anindya <anindyacse30@gmail.com>
2016-10-28 13:43:54 +0600

    Upload all files

---
+++ DeviceUID-Keychain/UniteDeviceUID/UniteDeviceUID.h
@@ -0,0 +1,13 @@
+
+//
+//  UniteDeviceUID.h
+//  Unite-Help
+//
+//  Created by Anindya Das on 8/3/16.
+//  Copyright © 2016 AppsInception. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+@interface UniteDeviceUID : NSObject
++ (NSString *)uid;
+@end
